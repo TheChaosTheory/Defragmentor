f09cc5dda32e8113a4ca65f38d3400f231573cae
tom <tom@Arturia>
2014-10-29 12:34:13 -0500

    Test

--- Lists/src/Defragment.java
+++ Lists/src/Defragment.java
@@ -6,7 +6,7 @@ public class Defragment implements DefragmenterInterface {
 	private ArraySortedList IDList;
 	private ArraySortedList CompletedList;
 	private int[] CompletedIDs;
-	private String jon = "I am a Test";
+	private String jon = "I am a Tes og oft";
 	public Defragment(){
 		IDList = new ArraySortedList();
 		CompletedList = new ArraySortedList();
